489742391caec0eb200505ca5dda39a65edb55c0
Tiziano Tomas Luzi Ramos <tizianoluziramos@gmail.com>
2025-07-31 15:22:54 -0300

    Restructured all the project and added 5 new games
    Co-authored-by: Project Manager <admin@perfect.wuaze.com>

--- src/APIS/Tools/index.ts
+++ src/APIS/Tools/index.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import gamerecommender from "./routes/gamerecommender.route";
 import indexController from "./controllers/index.controller";
-import gamedatabase from "./routes/gamedatabase.route";
 
 const router = Router();
 
 router.get("/", indexController.getRoutes);
 router.use("/gamerecommender", gamerecommender);
-router.use("/gamesdatabase", gamedatabase);
 
 export default router;
